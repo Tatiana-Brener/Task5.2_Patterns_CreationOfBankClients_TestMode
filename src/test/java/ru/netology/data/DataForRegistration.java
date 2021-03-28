ac0ce18d72da9670050a8c8f485512ba582c3103
Tatana <burmatova@inbox.ru>
2021-03-28 17:59:19 +0300

    added DataForRegistration.java

---
+++ src/test/java/ru/netology/data/DataForRegistration.java
@@ -0,0 +1,13 @@
+package ru.netology.data;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+
+public class DataForRegistration {
+    private final String login;
+    private final String password;
+    private final String status;
+}
